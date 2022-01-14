af1533ee769b8045850883ce7ebd26e9ed66eb5b
kimjunte <39764191+kimjunte@users.noreply.github.com>
2022-01-14 17:19:29 +0000

    add all files

---
+++ my-react-app/src/redux/ReduxCounter.js
@@ -0,0 +1,13 @@
+import CounterDisplay from "./CounterDisplay";
+import CounterIncrementor from "./CounterIncrementor";
+
+const ReduxCounter = () => {
+    return (
+        <>
+            <CounterDisplay/>
+            <CounterIncrementor/>
+        </>
+    );
+}
+
+export default ReduxCounter;
